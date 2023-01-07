fd6ab4e9a3c8b6d5a81ad18d2628442e8ae3f25f
linux <jeevithamurugan.2512@gmail.com>
2023-01-07 12:08:06 +0530

    placement.txt

---
+++ pt.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[]){
+        char ch='5';
+        if(ch>=48&&ch<=57){
+               System.out.println("its a digit");
+        }
+        else{
+            System.out.println("not a digit");
+        }
+        
+    }
+}
